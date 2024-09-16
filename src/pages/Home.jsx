82946ee87a5bde31d6691620ae53259fd73f9483
TomasTozzi <tozzitomas@gmail.com>
2024-09-16 15:27:13 -0300

    prueba2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,14 @@ import LogoRojo from "../assets/PNG's/Logotipos/AmouréSolo_3.Logotipo Rojo.png"
 function Home() {
   return (
     <div className="font-hat scroll-smooth ">
-      <div className="relative bg-home  bg-cover bg-center ">
+      <div className="relative bg-home2 bg-cover bg-center ">
         {/* Capa con opacidad */}
         <div className="absolute inset-0 bg-black opacity-65"></div>
         {/* Contenido */}
-        <div className="relative font-hat flex flex-col items-center justify-center z-10 py-3 h-[40vh]">
+        <div className="relative font-hat flex flex-col items-center justify-center z-10 mt-10  h-[40vh]">
           <img src={LogoRojo} alt="" className="h-24" />
           <p className="text-white text-center text-xl">
-            El primer <span className="text-celeste font-bold">'SI'</span> lo das con <span className="text-celeste font-bold">AMIGOS</span>{" "}
+            El primer <span className="text-rosa font-bold">'SI'</span> lo das con <span className="text-rosa font-bold">AMIGOS</span>{" "}
           </p>
           <button className="hidden">Contactanos</button>
         </div>
